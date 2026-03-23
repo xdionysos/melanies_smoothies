f89cf61b76d0f32f0406f24474f7d8437546ae9f
diony12 <143040068+xdionysos@users.noreply.github.com>
2026-03-23 11:00:23 +0100

    Update smoothie API response to show JSON data
    Change response handling for smoothie API call to display JSON.

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,5 +43,5 @@ if ingredients_list:
         st.success('Your Smoothie is ordered, ' + name_on_order + "!", icon="✅")
 
 import requests  
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-st.text(smoothiefroot_response)
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
+st.text(smoothiefroot_response.json())
